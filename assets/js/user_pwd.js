46645e3bedcddef60fb9c680991cfba5099dcfb2
xiaoyegecode <1010039036@qq.com>
2022-08-04 16:07:54 +0800

    密码部分完成

--- assets/js/user_pwd.js
+++ assets/js/user_pwd.js
@@ -1,2 +1,26 @@
 const form = layui.form;
 const layer = layui.layer;
+form.verify({
+  pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+  samePwd: (val) => {
+    if (val === $("[name=oldPwd]").val()) return "新旧密码不能相同！";
+  },
+  rePwd: (val) => {
+    if (val !== $("[name=newPwd]").val()) return "两次密码不一致！";
+  },
+});
+
+$('.layui-form').on('submit', function (e) {
+  e.preventDefault();
+  $.ajax({
+    type: 'POST',
+    url: '/my/updatepwd',
+    data: form.val('formPassword'),
+    success: res => {
+      // console.log(res);
+      const { status, message } = res
+      if (status !== 0) return layer.msg(message);
+      $(".layui-form")[0].reset();
+    }
+  })
+})
